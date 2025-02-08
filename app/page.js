ff58dc0c2b447a8cb4d7ceb53e0ae6bfde15de66
Kyle Ton <kyleton@Kyles-MacBook-Air-M3.local>
2025-02-08 13:24:23 -0800

    about me section text complete

--- app/page.js
+++ app/page.js
@@ -16,18 +16,16 @@ export default function Home() {
 
   return (
     <div>
-      <div className="flex justify-between items-center ml-12 mt-6">
+      <div id='Header' className="flex justify-between items-center ml-12 mt-6">
         <h1 className="font-semibold text-3xl">kyle ton</h1>
         <div className="mt-6 mr-12">
-          {/* Menu Container */}
           <div className="relative">
-            {/* Menu Items Container - Absolutely positioned */}
             <div className="absolute right-full top-1/2 -translate-y-1/2 flex items-center mr-4">
               {menuItems.map((item, index) => (
                 <button
                   key={item.title}
                   className={`
-                    mb-4 mr-6 text-lg font-medium
+                    mb-4 mr-8 text-lg font-medium
                     transition-all duration-500 ease-in-out
                     ${item.delay}
                     ${isMenuOpen 
@@ -50,6 +48,25 @@ export default function Home() {
             </button>
           </div>
         </div>
+      </div>
+      <div id='Biography' className='ml-20 mt-10 w-2/6'>
+          <h1 className='font-medium text-2xl mb-6'>hello, i'm . . .</h1>
+          <div className='ml-2 space-y-6'>
+            <p className='font-normal text-sm'>
+              kyle ton, a student at orange coast college studying computer science!
+              i am an aspiring software engineer with a passion for creating and developing full-stack applications.
+            </p>
+            <p className='font-normal text-sm'>
+              i have a large interest in machine learning and ai, particularly in language models and llms.
+              i want to one day specialize in natural language processing and be able to work on integrating nlp
+              alongside machine learning models.
+            </p>
+            <p className='font-normal text-sm'>
+              my goal for my career one day is to become a full-stack software engineer! i am beginning my full-stack journey,
+              building my skillsets and creating new projects! i hope one day, i can transition into machine learning and ai
+              engineer roles and make my mark on the tech industry! please feel free to contact me for anything at all!
+            </p>
+          </div>
       </div>
     </div>
   );
