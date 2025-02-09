7e60f4ecadd244acdfd34a2d53bc4cd12c0cb6ab
Kyle Ton <kyleton06@gmail.com>
2025-02-08 19:17:38 -0800

    going to begin working on vertical layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Enables static export
+  images: {
+    unoptimized: true, // Disable Next.js image optimization for GitHub Pages
+  },
+};
 
 export default nextConfig;
