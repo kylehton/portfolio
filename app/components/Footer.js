f07a80a476ba2302607fcdbff6d5873c414b2156
Kyle Ton <kyleton06@gmail.com>
2025-02-09 21:57:52 -0800

    finished footer section and made github calendar same font and text as theme, now working on adding framer motion

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,8 +2,10 @@
 export default function Footer() {
 
     return (
-        <div className='mb-32'>
-
+        <div className='mt-10 mb-10 w-full flex justify-end'>
+            <p className='mr-5 lg:mt-20 lg:mr-10 text-[10px] text-right text-zinc-500 w-2/5 lg:w-1/5'>
+                thank you for visiting my website! it was made completely from scratch using tailwindcss, reactjs, and framer motion.
+            </p>
         </div>
     )
 }
