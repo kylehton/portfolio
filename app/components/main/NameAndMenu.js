5be71c7c31cfa72211121b6b333463d6e6188a9a
Kyle Ton <kyleton06@gmail.com>
2025-02-10 22:13:54 -0800

    should be ready for all devices (desktop, tablet, phone)

--- app/components/main/NameAndMenu.js
+++ app/components/main/NameAndMenu.js
@@ -2,9 +2,7 @@
 import { Menu } from 'lucide-react';
 import React, { useState } from 'react';
 
-
 const NameAndMenu = () => {
-
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const menuItems = [
@@ -14,49 +12,48 @@ const NameAndMenu = () => {
     ];
 
     return (
-
-        <header className="2xl:mt-4 px-4 lg:px-12 py-6">
-            <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center">
-            {/* Title content */}
-            <div className="order-2 lg:order-1 flex items-center mt-6 lg:mt-0">
-                <h1 className="font-semibold text-2xl lg:text-3xl">kyle ton</h1>
-                <span className="text-xl font-normal mx-4 text-zinc-600">|</span>
-                <span className="mt-1 text-sm font-light text-zinc-500">
-                student and software engineer
-                </span>
-            </div>
-
-            {/* Menu Container */}
-            <div id="menu-container" className="order-1 lg:order-2 relative flex justify-end lg:block">
-                {/* Menu Items */}
-                <div className="absolute right-8 lg:right-full top-1/2 -translate-y-1/2 flex items-center lg:mr-4">
-                {menuItems.map((item) => (
-                    <a
-                    key={item.title}
-                    href={item.link}
-                    className={`
-                        mr-4 lg:mr-8 text-sm lg:text-lg font-normal text-zinc-600
-                        transition-all duration-500 ease-in-out
-                        ${item.delay}
-                        ${isMenuOpen 
-                        ? 'opacity-100 translate-x-0' 
-                        : 'opacity-0 translate-x-16 pointer-events-none'
-                        }
-                    `}
-                    >
-                    {item.title}
-                    </a>
-                ))}
+        <header className="sm:mt-4 2xl:mt-4 px-4 lg:px-12 py-4">
+            <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center">
+                {/* Title content */}
+                <div className="order-2 sm:order-1 flex items-center mt-6 sm:mt-0">
+                    <h1 className="font-semibold text-2xl lg:text-3xl">kyle ton</h1>
+                    <span className="text-xl font-normal mx-4 text-zinc-600">|</span>
+                    <span className="mt-1 text-sm font-light text-zinc-500">
+                        student and software engineer
+                    </span>
                 </div>
-                {/* Menu Button */}
-                <button 
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-                className="focus:outline-none relative z-10"
-                aria-label="Toggle menu"
-                >
-                <Menu className="mt-2 h-4 w-4 lg:h-6 lg:w-6" />
-                </button>
-            </div>
+
+                {/* Menu Container */}
+                <div id="menu-container" className="order-1 sm:order-2 relative flex justify-end">
+                    {/* Menu Items */}
+                    <div className="absolute right-8 lg:right-full top-1/2 -translate-y-1/2 flex items-center lg:mr-4">
+                        {menuItems.map((item) => (
+                            <a
+                                key={item.title}
+                                href={item.link}
+                                className={`
+                                    mr-4 lg:mr-8 text-sm lg:text-lg font-normal text-zinc-600
+                                    transition-all duration-500 ease-in-out
+                                    ${item.delay}
+                                    ${isMenuOpen 
+                                        ? 'opacity-100 translate-x-0' 
+                                        : 'opacity-0 translate-x-16 pointer-events-none'
+                                    }
+                                `}
+                            >
+                                {item.title}
+                            </a>
+                        ))}
+                    </div>
+                    {/* Menu Button */}
+                    <button 
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        className="focus:outline-none relative z-10"
+                        aria-label="Toggle menu"
+                    >
+                        <Menu className="mt-2 h-4 w-4 lg:h-6 lg:w-6" />
+                    </button>
+                </div>
             </div>
         </header>
     );
