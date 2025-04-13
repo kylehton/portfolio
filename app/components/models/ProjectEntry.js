e064d3cb6b091443da2e97e5ecf3a716248aed03
Kyle Ton <kyleton06@gmail.com>
2025-04-13 15:32:01 -0700

    updated everything

--- app/components/models/ProjectEntry.js
+++ app/components/models/ProjectEntry.js
@@ -11,7 +11,7 @@ const ProjectEntry = ({ content }) => {
                         <div id="div-to-hold-headers">
                         <h4 className="font-medium text-[19.5px]">{content.title}</h4>
                         <div className="text-sm grid grid-cols-1 lg:grid-cols-2">
-                            <p>{content.subheading}</p>
+                            <p className="mb-1">{content.subheading}</p>
                             <p className="lg:text-right text-xs">{content.dates}</p>
                         </div>
                         </div>
@@ -20,10 +20,10 @@ const ProjectEntry = ({ content }) => {
                         className="mt-3 ml-2 lg:w-4/5 font-light text-sm whitespace-pre-line">
                         {content.description}
                         </p>
-                        <div className='mt-2 font-light text-lg text-zinc-700 flex flex-col lg:flex-row justify-end'>
-                            <a className="ml-4 mb-1" rel='external noopener noreferrer' href={content.video}>[video demo]</a>
-                            <a className="ml-4 mb-1" rel='external noopener noreferrer' href={content.deployment}>[live deployment]</a>
-                            <a className="ml-4 mb-1" rel='external noopener noreferrer' href={content.github}>[github repo]</a>
+                        <div className='mt-2 font-light text-[17px] text-zinc-700 flex flex-col lg:flex-row justify-end'>
+                            <a className="lg:ml-4 ml-2 mb-1" target='_blank' rel='external noopener noreferrer' href={content.video}>[video demo]</a>
+                            <a className="lg:ml-4 ml-2 mb-1" target='_blank' rel='external noopener noreferrer' href={content.deployment}>[live deployment]</a>
+                            <a className="lg:ml-4 ml-2 mb-1" target='_blank' rel='external noopener noreferrer' href={content.github}>[github repo]</a>
                         </div>
                     </div>
                 }
