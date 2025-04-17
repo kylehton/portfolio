61eef5191cd168d9c3d42f640bc45487c75179c3
Kyle Ton <kyleton@Kyles-MacBook-Air-M3.local>
2025-04-17 10:22:56 -0700

    trying out new changes to projects section, playing around with font weights in various places

--- app/components/main/Footer.js
+++ app/components/main/Footer.js
@@ -3,8 +3,8 @@ const Footer = () => {
 
     return (
         <div className='mt-10 mb-8 w-full flex justify-end'>
-            <p className='mr-5 lg:mt-20 lg:mr-10 text-[10px] text-right text-zinc-500 w-2/5 lg:w-1/5'>
-                thank you for visiting my website! it was made completely from scratch using tailwindcss, next.js, shadcn, and framer motion.
+            <p className='font-extralight mr-5 lg:mt-20 lg:mr-10 text-[10px] text-right text-zinc-500 w-2/5 lg:w-1/5'>
+                thank you for visiting my website! it was made completely from scratch using tailwindcss, next.js, shadcn, magic ui, and framer motion.
             </p>
         </div>
     )
