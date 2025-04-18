9ec70fea9110b300945fcc26c5128023a28287a9
Kyle Ton <kyleton@Kyles-MacBook-Air-M3.local>
2025-04-18 16:03:51 -0700

    going to add pictures for about me section

--- app/components/main/AboutMe.js
+++ app/components/main/AboutMe.js
@@ -2,6 +2,14 @@ import ScrollMotionWrapper from "@/app/motion/ScrollMotionWrapper";
 
 const AboutMe = () => {
 
+    // arrays to hold file names for pics
+    const personalContent = [];
+    const [currentPersonalContent, setCurrentPersonalContent] = useState(0);
+    const foodContent = [];
+    const [currentFoodContent, setCurrentFoodContent] = useState(0);
+    const bookContent = [];
+    const [currentBookContent, setCurrentBookContent] = useState(0);
+
     return (
         <main id='about-me' className="mb-8 px-4 lg:px-16 py-8">
             <div className="flex flex-col lg:flex-row gap-8 lg:gap-16">
@@ -13,8 +21,21 @@ const AboutMe = () => {
                     </ScrollMotionWrapper>
                     <ScrollMotionWrapper
                         contents={
-                            <div id='about-me-content' className="ml-1 font-light text-[17px] space-y-1">
-                                <p>non-cs related personalized about me section coming soon!!!</p>
+                            <div id='about-me-content' className="ml-1 font-light space-y-1">
+                                <div id="personal-content" className="flex flex-col lg:flex-row">
+                                    <p className='text-[17px]'>
+                                        i like spending time with friends and family, doing whatever really.
+                                        also, ive been playing pickleball for the last year and a half throughout college.
+                                        lmk if youre down to play!
+                                    </p>
+                                    <div id="personal-content-img" className="">
+                                        <img 
+                                            src={personalContent[currentPersonalContent]} 
+                                            alt={""}
+                                            className="w-full h-full object-cover rounded-lg border-2 border-zinc-500"
+                                        />
+                                    </div>
+                                </div>
                             </div>
                         }>
                     </ScrollMotionWrapper>
