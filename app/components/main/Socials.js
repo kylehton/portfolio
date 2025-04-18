7b646207784f1e8d208f19ec505aa3fc8c6648d1
Kyle Ton <kyleton@Kyles-MacBook-Air-M3.local>
2025-04-17 22:25:41 -0700

    did not realize there was overflow from the <a> element for socials buttons, put a band aid on it might do a better fix if needed

--- app/components/main/Socials.js
+++ app/components/main/Socials.js
@@ -5,8 +5,7 @@ import { Button } from '@/components/ui/button';
 
 const Socials = () => {
 
-    return (
-
+    return (  
         <main id='socials' className="px-4 lg:px-16 py-8">
         <div className="flex flex-col lg:flex-row gap-8 lg:gap-16">
         <div className='lg:min-w-full'>
@@ -22,21 +21,21 @@ const Socials = () => {
               <ScrollMotionWrapper 
                 contents={
                     <div id='my-social-links' className='flex flex-col font-normal text-[18px] text-zinc-500'> 
-                        <a target='_blank' href='https://www.linkedin.com/in/kyle-ton/' >
+                        <a target='_blank' href='https://www.linkedin.com/in/kyle-ton/' className='w-min'>
                             <Button className="bg-[#0A66C2] text-white font-bold py-2 px-4 lg:py-2 lg:px-5 border-2 border-white rounded-md 
                                   shadow-[3px_3px_0px_0px_gray] hover:shadow-[1px_1px_0px_0px_white] hover:translate-x-[2px] 
                                   hover:translate-y-[2px] mt-3 hover:scale-[1.02] hover:bg-[#004182] transition-all mr-4">
                                       linkedin
                             </Button>
                         </a>
-                        <a target='_blank' href='https://www.github.com/kylehton/' >
+                        <a target='_blank' href='https://www.github.com/kylehton/' className='w-min'>
                             <Button className="bg-zinc-700 text-white font-bold py-2 px-5 lg:py-2 lg:px-6 border-2 border-white rounded-md 
                                   shadow-[3px_3px_0px_0px_black] hover:shadow-[1px_1px_0px_0px_white] hover:translate-x-[2px] 
                                   hover:translate-y-[2px] mt-4 hover:scale-[1.02] hover:bg-[#5FA85F] transition-all mr-4">
                                         github
                             </Button>
                         </a>
-                        <a target='_blank' href='https://www.devpost.com/kylehton/'>
+                        <a target='_blank' href='https://www.devpost.com/kylehton/' className='w-min'>
                             <Button className="bg-[#12394c] text-white font-bold py-2 px-4 lg:py-2 lg:px-5 border-2 border-white rounded-md 
                                     shadow-[3px_3px_0px_0px_#6BD1EC] hover:shadow-[1px_1px_0px_0px_white] hover:translate-x-[2px] 
                                     hover:translate-y-[2px] mt-4 hover:scale-[1.02] hover:bg-[#3BA29D] transition-all mr-4">
