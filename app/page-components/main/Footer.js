087e2f97afad0e9893c943f10e5b090f6092f23a
Kyle Ton <kyleton06@gmail.com>
2025-12-15 18:48:49 -0800

    installed latest nextjs version

--- app/page-components/main/Footer.js
+++ app/page-components/main/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className='mt-10 mb-8 w-full flex justify-end'>
                 <p className='flex flex-col font-extralight mr-5 lg:mt-20 lg:mr-10 text-[10px] text-right text-zinc-500 w-2/5 lg:w-1/5'>
                     thank you for visiting my website! it was made completely from scratch using tailwindcss, next.js, shadcn, magic ui, and framer motion.
-                    <span className='font-extralight text-[10px] text-zinc-400'>last updated: dec 15 2025, 6:38pm pst;</span>
+                    <span className='font-extralight text-[10px] text-zinc-400'>last updated: dec 15 2025, 6:48pm pst;</span>
                 </p>
                 
         </div>
